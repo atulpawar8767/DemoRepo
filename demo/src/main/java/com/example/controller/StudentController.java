139a4bf2f99ed51f8b715c427d5250ca0fbf5fbb
Atul <atulpawar8767@gmail.com>
2023-07-09 09:40:19 +0530

    Second success commit

--- demo/src/main/java/com/example/controller/StudentController.java
+++ demo/src/main/java/com/example/controller/StudentController.java
@@ -1,12 +1,9 @@
 package com.example.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.entity.Student;
 import com.example.service.StudentService;
-
 import jakarta.validation.Valid;
 
 @RestController
@@ -57,7 +52,6 @@ public class StudentController {
 	@GetMapping("/email")
 	public ResponseEntity<Boolean> isStudentValid(@RequestParam(name = "email") String email) {
 		boolean studentByEmail = studentService.getStudentByEmail(email);
-		System.out.println("Hello");
 		return new ResponseEntity<Boolean>(studentByEmail, HttpStatus.OK);
 	}
 	
